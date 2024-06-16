7fba880cf94f03defff5d02eb52cb13d1db1857e
DhruvamUnikon <148038962+DhruvamUnikon@users.noreply.github.com>
2024-06-16 19:58:38 +0530

    added required details for calling

--- lib/src/invitation/defines.dart
+++ lib/src/invitation/defines.dart
@@ -59,10 +59,26 @@ class ZegoCallInvitationData {
 class ZegoCallUser {
   String id;
   String name;
+  String? profilePicture;
+  String? currentDesignation;
+  String? currentOrganization;
 
-  ZegoCallUser(this.id, this.name);
+  ZegoCallUser(
+    this.id,
+    this.name, {
+    this.profilePicture,
+    this.currentDesignation,
+    this.currentOrganization,
+  });
 
-  ZegoCallUser.fromUIKit(ZegoUIKitUser user) : this(user.id, user.name);
+  ZegoCallUser.fromUIKit(ZegoUIKitUser user)
+      : this(
+          user.id,
+          user.name,
+          profilePicture: user.profilePicture,
+          currentDesignation: user.currentDesignation,
+          currentOrganization: user.currentOrganization,
+        );
 
   @override
   String toString() {
